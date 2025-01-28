ffcb1612aa5da5914bd2da12d764ad737f611f94
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-01-28 09:51:40 -0700

    Took Notes

--- Variables/notes.py
+++ Variables/notes.py
@@ -1,1 +1,16 @@
 # Ryan Rowbury, Variables Notes
+
+name = "Ryan The Cool"
+
+print(name, ", The sigma, The Slicer,")
+
+name = "Ryan The Skibidi"
+
+print(name)
+
+max = 20
+min = 10
+print(min,"|",max)
+
+name = input("please tell me you name: ")
+print(name+ ", welcome to my progam.")
