350b57b1b560e75efda71993031172b26692f2dc
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-01-29 09:54:05 -0700

    Wrote some code

---
+++ Variables/Practice.py
@@ -0,0 +1,25 @@
+#Ryan R, Variables Practice Python
+
+name=("Ryan")
+nub10_var=("3")
+nub100_var=("203")
+break_var=("Protein Shake")
+color_var=("Orange")
+name_S_var=("UCAS")
+year=("2025")
+eye_color=("Brown")
+age_var=("16")
+Fav_S_Sub_var=("Am Civ")
+
+
+
+print(name)
+print(nub10_var)
+print(nub100_var)
+print(break_var)
+print(color_var)
+print(name_S_var)
+print(year)
+print(eye_color)
+print(age_var)
+print(Fav_S_Sub_var)
--- Variables/notes.py
+++ Variables/notes.py
@@ -1,16 +1,8 @@
 # Ryan Rowbury, Variables Notes
 
-name = "Ryan The Cool"
-
-print(name, ", The sigma, The Slicer,")
-
-name = "Ryan The Skibidi"
-
-print(name)
-
-max = 20
-min = 10
-print(min,"|",max)
-
 name = input("please tell me you name: ")
 print(name+ ", welcome to my progam.")
+
+name=input("Run Program?:")
+if(name == "Y") :print("The program doesn't work yet")
+if(name == "N") :print("Okay restarting Program")
