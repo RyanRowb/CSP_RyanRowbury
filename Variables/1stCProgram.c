efa942a65dc4e1e7707c979a2a3abed923ad2f75
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-01-31 09:49:24 -0700

    Variable Practice

---
+++ Variables/1stCProgram.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+varbug
+
+int main(void){
+    
+}
---
+++ Variables/Varbiles.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+char name[]="Ryan";
+int num = 7;
+int lnum = 409;
+char brefa[]="Handful of Blue Berries";
+char color[]= "Orange";
+char schname[]="UCAS";
+char year[]="2025";
+char eyec[]="Brown";
+int agem = 16;
+char favschsub[]="Am Civ";
+
+
+
+int main(void){
+    printf("%s\n", name);
+    printf("%d\n", num);
+    printf("%d\n", lnum);
+    printf("%s\n", brefa);
+    printf("%s\n", color);
+    printf("%s\n", schname);
+    printf("%s\n", year);
+    printf("%s\n", eyec);
+    printf("%d\n", agem);
+    printf("%s\n", favschsub);
+    return 0;
+}
