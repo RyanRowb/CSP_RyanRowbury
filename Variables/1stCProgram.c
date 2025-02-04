433d63b8c49f14471948e5054863aa4202500775
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-02-04 09:19:02 -0700

    Made it

--- Variables/1stCProgram.c
+++ Variables/1stCProgram.c
@@ -1,7 +1,10 @@
 //Ryan Rowbury, 1stCProgram
 #include <stdio.h>
-
+int greet;
 
 int main(void){
-    
+    printf("What is your favorite Number?\n");
+    scanf("%d", &greet);
+    printf("%d", greet);
+    printf(", Thats a good number!");
 }
