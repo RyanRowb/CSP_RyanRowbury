47f3f6c0d1d3b7dc83ee1676309c306a93431163
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-02-05 09:29:49 -0700

    Finished Expressions C++

--- Variables/ExpresionPractice.c
+++ Variables/ExpresionPractice.c
@@ -6,8 +6,20 @@ int e1 = 7-24/8*4+6;
 int e2 = 18/3-7+2*5;
 int e3 = 6*4/12+72/8-9;
 int e4 = 17-6/2+4*3;
-int e5 = -2*1*4*-2*2/2*2+6+2-3;
+int e5 = -2*(1*4-2/2)+(6+2-3);
+int e6 = -1*((3-4*7)/5)-2*24/6;
+int e7 = (3*(int) pow(5, 2)/15)-(5-(int) pow(2, 2));
+int e8 = ((int) pow(1, 4)*(int) pow(2, 2)+(int) pow(3, 3))-(int) pow(2, 5)/4;
+int e9 = ((int) pow(22/2-2*5, 2))+((int) pow(4-6/6, 2));
 
 int main(void){
     printf("%d\n", e1);
+    printf("%d\n", e2);
+    printf("%d\n", e3);
+    printf("%d\n", e4);
+    printf("%d\n", e5);
+    printf("%d\n", e6);
+    printf("%d\n", e7);
+    printf("%d\n", e8);
+    printf("%d\n", e9);
 }
