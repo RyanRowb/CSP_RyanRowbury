33434a008e1dd38937e240cf85148d875cc220bc
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-02-06 09:48:28 -0700

    F

--- Work/Expressions/Financial_Calc.py
+++ Work/Expressions/Financial_Calc.py
@@ -1,1 +1,29 @@
 #Ryan R, Financial calculator Python
+
+#Write a print statment telling the user what hte program is (budget cacl)
+print("Welcome to the Budget Caculator.")
+#Ask for monthly income (user input)
+income = float( input("What is your Monthly Income?:"))
+# Ask for rent (user input)
+
+#Ask for utilities (user input)
+
+#Ask for groceries (user input)
+
+#Ask for Transportation (user input)
+
+#Calculate savings as 10% of income (variable)
+
+#How much spending money they have left (Income-everything eles)
+
+#Caculate the perecent of rent (Variables)
+
+#Caculate the perecent of utilities (Variables)
+
+#Caculate the perecent of groceries (Variables)
+
+#Caculate the perecent of transportation (Variables)
+
+#Tell user category spedning amount and percent Ex. ("You spend $XX.XX of your monthly wage on rent.")
+
+#percent and aount of spending money
--- Work/Expressions/Financial_calc.c
+++ Work/Expressions/Financial_calc.c
@@ -1,4 +1,35 @@
 //Ryan R, Financial Calculator
+
+//Ryan R, Financial calculator Python
+
+//Write a print statment telling the user what hte program is (budget cacl)
+
+//Ask for monthly income (user input)
+
+// Ask for rent (user input)
+
+//Ask for utilities (user input)
+
+//Ask for groceries (user input)
+
+//Ask for Transportation (user input)
+
+//Calculate savings as 10% of income (variable)
+
+//How much spending money they have left (Income-everything eles)
+
+//Caculate the perecent of rent (Variables)
+
+//Caculate the perecent of utilities (Variables)
+
+//Caculate the perecent of groceries (Variables)
+
+//Caculate the perecent of transportation (Variables)
+
+//Tell user category spedning amount and percent Ex. ("You spend $XX.XX of your monthly wage on rent.")
+
+//percent and aount of spending money
+
 #include <stdio.h>
 
 
