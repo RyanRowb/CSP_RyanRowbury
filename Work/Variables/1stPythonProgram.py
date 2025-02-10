36e41d0184152ff8468fcaba8c5f913fbb4c81de
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-02-10 09:53:32 -0700

    Worked on game

--- Work/Variables/1stPythonProgram.py
+++ Work/Variables/1stPythonProgram.py
@@ -8,7 +8,7 @@ if(q_var == "N") :print("Okay, stopping Program")
 if(q_var == "Y") :pname = input("What is your Character's name?\n")
 g_var = input(f"Where would you like to start {pname},\n1. A tavern in a town \n2. The Enchanted Forest \n3. The Mountains of Halcion? ")
 
-if(g_var == "1"): print("\nYou awake in a bed at the local tavern.")
+if(g_var == "1"): print(f"\nYou awake in a bed at the local tavern.")
 if(g_var == "2"): print("\nYou find yourself in the Magical Forest. You don't know how you got here, but its warm glow and fresh smell make you want to stay.")
 if(g_var == "3"): print("\nYou find yourself in the High Mountains of Halcion, it is cold and very harsh. Your failing camp fire won't do good against this lasting cold.")
 print("What do you do?\n")
@@ -17,4 +17,5 @@ if(g_var == "2"): for_var = input("1. Leave Forest\n2. Look around Forest\n3. Fi
 if(g_var == "3"): mou_var = input("1. Try to climb down the mountain\n2. Try to restart the Campfire\n Look for something like a cave\n")
 
 if(tav_var == "1"): print("You leave the safe room and enter the empty tavern. Your door slams behind you.")
-if(tav_var == "2"): print("yes")
+if(tav_var == "2"): print("You see nothing")
+if(tav_var == "3"): print ("You sleep longer and awake in a fire.\n GAME OVER")
