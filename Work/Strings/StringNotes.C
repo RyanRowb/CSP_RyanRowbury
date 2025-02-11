b53731e7c52c2df8ce5866788ac5de2371274fd8
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-02-11 09:45:46 -0700

    Notes

--- Work/Strings/StringNotes.C
+++ Work/Strings/StringNotes.C
@@ -1,9 +1,23 @@
 //Ryan Rowbury, String Notes
 #include <stdio.h>
+#include <string.h>
 
 char name[20];
 
 int main(void){
-    printf("Please tell me your full name\n");
-    scanf("%s", name);
+    //printf("Please tell me your full name\n");
+    //scanf("%s", name);
+    //fgets(name, 20, stdin);
+    //printf("Hello %s, welcome to my program", name);
+    //char sentance[] = "\nThe quick brown fox jumps over the lazy dog";
+    //printf("%d\n", strlen(sentance));
+    char one[] = "Hello ";
+    char two[] = "World!";
+    char three[]= "This is my program ";
+    printf("%s\n", one);
+    strcat(one, two);
+    printf("%s\n", one);
+    strcat(three, two);
+    printf("%s\n", three);
+    return(0);
 }
