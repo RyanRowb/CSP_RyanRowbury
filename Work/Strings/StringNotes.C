50f101b2b119bcda2c8c54eec8d62cc0fe3ea3a4
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-02-11 09:20:35 -0700

    Finished notes py

---
+++ Work/Strings/StringNotes.C
@@ -0,0 +1,9 @@
+//Ryan Rowbury, String Notes
+#include <stdio.h>
+
+char name[20];
+
+int main(void){
+    printf("Please tell me your full name\n");
+    scanf("%s", name);
+}
--- Work/Strings/StringNotes.py
+++ Work/Strings/StringNotes.py
@@ -2,14 +2,24 @@
 
 # words in programing (datat type) "" or ''
 
-print("Hello Me")
-print('Hello Marrio')
+#print("Hello Me")
+#print('Hello Marrio')
 
 #some errors include
 #print('Ryan's code')
 
 #Stupid Proofing
 
-ex = "      HELlo my nAMe is Ryan".strip().lower()
-print("What is your name\n")
-print(f"Hello {ex}, Welcome to my program.")
+#ex = "      HELlo my nAMe is Ryan".strip().lower()
+#print("What is your name\n")
+#print(f"Hello {ex}, Welcome to my program.")
+
+#sentence = "The qucik brown fox jumps over the lazy dog."
+#word = sentence.find("fox")
+
+#name = "Ryan"
+#print("Welcome to the program "+name)
+#or
+#print(f"Welcome to my program {name}")
+percent = 89
+print(f"your grade is {percent:.2f}")
