bb0e025c50f540a864f82b9bf702f9a11dd14c69
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-02-12 09:14:40 -0700

    Finished Name Dec

--- Work/Strings/Practice.py
+++ Work/Strings/Practice.py
@@ -1,5 +1,7 @@
 name = input("What is your first name?\n").lower().strip().capitalize()
 print(f"Hello {name}, welcome to the name decorator.")
-decor = input("How would you like to decorate your name? (Just type the number) \n1. xX name Xx\n2. (:(: name :) :)\n3. !? name !?\n") .strip()
+decor = input("How would you like to decorate your name? (Just type the number) \n1. xXNameXx\n2. (:(: Name :) :)\n3. !?Name!?\n Number:") .strip()
 
-if(decor == "1"): print(f"xX{name}Xx")
+if(decor == "1"): print(f"Here is your decorated name, xX{name}Xx")
+if(decor == "2"): print(f"Here is your decorated name, (:(: {name} :) :)")
+if(decor == "3"): print(f"Here is your decorated name, !?{name}!?")
