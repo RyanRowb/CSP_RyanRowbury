8863957d9b135e622c2567692d0946696a2a4f7b
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-02-14 09:23:11 -0700

    finished Python ISlly

--- Work/Strings/SillySentance.py
+++ Work/Strings/SillySentance.py
@@ -1,1 +1,8 @@
 #Ryan R, Silly Sentances
+
+name = input("What is your name?\n")  .strip() .lower() .capitalize()
+print(f"Hello {name}, welcome to my program")
+charcter = input("Give me a random character\n").strip() .lower() .capitalize()
+action = input("Give me a random action (ending in ing)\n").strip() .lower()
+color = input("Give me a random color\n").strip() .lower()
+print(f"When {name} went to find the {charcter}, he found it by {action} and gave it a {color} flower.")
