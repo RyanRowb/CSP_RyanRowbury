315c2f146a85635b0f854e134b7078c99cedc9c8
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-02-21 09:32:36 -0700

    Finished UpdCalPy

--- Work/Functions/Financialcalc.py
+++ Work/Functions/Financialcalc.py
@@ -4,21 +4,24 @@ def info(income, amount, type):
    pertype = amount/income*100
    print(f"You send ${amount:.2f} on {type} and that is {pertype:.2f}% of your income")
 
-def minc
+def expen(type):
+   return float(input(f"What is your {type} per month\n"))
 
 print("Welcome to the Budget Caculator.")
-income = float( input("What is your Monthly Income?\n:"))
-rent = float(input("What do you pay in rent per month?:\n"))
-utilities = float(input("What do you pay in utilities per month?:\n"))
-groceries = float(input("What do you pay in groceries per month?:\n"))
-transportation = float(input("What do you pay in transportation per month?:\n"))
-savings = income*.1
-print(f"You should save 10% or, ${round(savings, 2)} dollars a month")
+income = expen("income")
+rent = expen("rent")
+utilities = expen("utilities")
+groceries = expen("groceries")
+transportation = expen("transportation")
+savings = (income/10)
+
+print(f"You should save 10% or, ${savings:.2f} dollars a month")
 info(income, rent, "rent")
 info(income, utilities, "utilities")
 info(income, groceries, "groceries")
 info(income, transportation, "transportation")
 
-print(f"This means you have ${round(spen, 2)} to spend.")
+spen = income - rent - utilities - groceries - transportation - savings
+print(f"This means you have ${round(spen, 2)} to spend if you don't want to save more.")
 spen_ver = (spen/income)*100
 print(f"This equates to {round(spen_ver , 2)}% a month.")
