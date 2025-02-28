4066a4ca8df1f0b531f7782fa6cc72b357b082db
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-02-28 09:36:32 -0700

    Finished

--- Work/Conditions/timegreeter.py
+++ Work/Conditions/timegreeter.py
@@ -5,11 +5,14 @@ curent =time.time()
 local_time = time.localtime(curent)
 hour = local_time.tm_hour
 
-name = input("What is your name?")
+name = input("What is your name?\n").strip().lower().capitalize()
 if (6 < hour < 11):
-    print("Good Morning")
+    print(f"Good Morning {name}")
 elif (hour < 14):
-    print("Good Afternoon")
+    print("Good Afternoon {name}")
 elif (hour <20):
-    print("Good Night, sleep tight, don't let the bed bugs bite")
-elif (hour <6 )
+    print("Good Night {name}, sleep tight, don't let the bed bugs bite")
+elif (hour<6) or (hour > 24):
+    print("GO TO BED {name}")
+else:
+    print("You are beyond time itself")
