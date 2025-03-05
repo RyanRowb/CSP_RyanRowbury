b250e83ee8bc5401c978681cf566d4aac3193e57
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-03-05 09:17:45 -0700

    Finihsed

---
+++ Work/Loops/loops.c
@@ -0,0 +1,15 @@
+//Ryan Rowbury, Loops Notes
+#include <stdio.h>
+int x;
+int i = 1;
+
+int main(void){
+for(x=0; x<10;x++){
+    printf("Hello\n");
+}
+while(i<10){
+    printf("%d\n", i);
+    i++;
+}
+return(0);
+}
