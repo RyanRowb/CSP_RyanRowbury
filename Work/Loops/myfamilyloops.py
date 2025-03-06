6000cfa7bbdc0cb3a9859d68c92a2aa7d7b17a9a
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-03-06 09:47:34 -0700

    Finished

---
+++ Work/Loops/myfamilyloops.py
@@ -0,0 +1,7 @@
+#Ryan R, Family Loops
+
+siblings = ["Adam", "Ethan", "Carmen", "Isaac", "Jason", "Jennifer", "Justin"]
+num = 1
+for siblings in siblings:
+    print(f"Hello {siblings}, welcome to my program!")
+    num += 1
