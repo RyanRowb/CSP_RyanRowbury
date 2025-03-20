534c8125eb401d37172937798fdeaf05bf80a675
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-03-20 09:53:08 -0600

    Need to work on it

--- Work/final.py
+++ Work/final.py
@@ -1,9 +1,30 @@
-#Ryan R (The cool one)
+#Ryan R (The cool one) Win detector!
 
-changemelater_var = ["1", "2", "3"]
-changemelater_var2 = ["4", "5", "6"]
-changemelater_var3 = ["7", "8", "9"]
+#grid
+grid = [[1,2,3],
+[4,5,6],
+[7,8,9]]
 
-print(changemelater_var)
-print(changemelater_var2)
-print(changemelater_var3)
+for grid in grid:
+    print(f" {grid[0]} | {grid[1]} | {grid[2]}\n --+---+--")
+
+grid = [["X",2,3],
+["X",5,6],
+["X",8,9]]
+#Ryan R (The cool one) Win detector!
+def win(grid):
+    if grid[0]==["X","X","X"]:
+        print("X wins")
+    elif grid[1]==["X", "X", "X"]:
+        print("X wins")
+    elif grid[2]==["X", "X", "X"]:
+        print("X wins")
+    elif grid[0]==["O", "O", "O"]:
+        print("O wins")
+    elif grid[1]==["O", "O", "O"]:
+        print("O wins")
+    elif grid[2]==["O", "O", "O"]:
+        print("O wins")
+    elif grid[0][1][2]==["X"]:
+        print("X wins")
+win(grid)
