b6a8211595b87d42c0cf61367842f7f82aa74aff
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-03-21 09:52:58 -0600

    I hate Tic Tac Toe

--- Work/final.py
+++ Work/final.py
@@ -8,9 +8,11 @@ grid = [[1,2,3],
 for grid in grid:
     print(f" {grid[0]} | {grid[1]} | {grid[2]}\n --+---+--")
 
-grid = [["X",2,3],
-["X",5,6],
-["X",8,9]]
+grid = [["O",2,3],
+        ["X","O",6],
+        ["X",8,"O"]]
+
+print(grid[2][0])
 #Ryan R (The cool one) Win detector!
 def win(grid):
     if grid[0]==["X","X","X"]:
@@ -25,6 +27,27 @@ def win(grid):
         print("O wins")
     elif grid[2]==["O", "O", "O"]:
         print("O wins")
-    elif grid[0][1][2]==["X"]:
+    elif grid[0][0]=="X" and grid[1][0]=="X" and grid[2][0]=="X":
         print("X wins")
+    elif grid[0][1]=="X" and grid[1][2]=="X" and grid[2][3]=="X":
+        print("X wins")
+    elif grid[0][2]=="X" and grid[1][2]=="X" and grid[2][2]=="X":
+        print("X wins")
+    elif grid[0][0]=="X" and grid[1][1]=="X" and grid[2][2]=="X":
+        print("X wins")
+    elif grid[0][2]=="X" and grid[1][1]=="X" and grid[2][0]=="X":
+        print("X wins")
+    elif grid[0][0]=="O" and grid[1][0]=="O" and grid[2][0]=="O":
+        print("O wins")
+    elif grid[0][1]=="O" and grid[1][2]=="O" and grid[2][3]=="O":
+        print("O wins")
+    elif grid[0][2]=="O" and grid[1][2]=="O" and grid[2][2]=="O":
+        print("O wins")
+    elif grid[0][0]=="O" and grid[1][1]=="O" and grid[2][2]=="O":
+        print("O wins")
+    elif grid[0][2]=="O" and grid[1][1]=="O" and grid[2][0]=="O":
+        print("O wins")
+    elif taken.append(spot)
+    else:
+        print("You tied")
 win(grid)
