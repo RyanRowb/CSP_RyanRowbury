8ed0db72d7e5d5cd66c7f0d227b894b458262f78
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-03-26 09:52:46 -0600

    I don't <3 coding

--- Work/final(mypart).py
+++ Work/final(mypart).py
@@ -2,25 +2,25 @@ def win(grid):
         if  grid[0][0] == "X" and grid[0][1]=="X" and grid[0][2]=="X":
             print("X wins")
             set (game_finsished = True)
-        elif grid[1]==["X", "X", "X"]:
+        elif grid[1][0] == "X" and grid[1][1]=="X" and grid[1][2]=="X":
             print("X wins")
             set (game_finsished = True)
-        elif grid[2]==["X", "X", "X"]:
+        elif grid[2][0] == "X" and grid[2][1]=="X" and grid[2][2]=="X":
             print("X wins")
             set (game_finsished = True)
-        elif grid[0]==["O", "O", "O"]:
+        elif grid[0][0] == "O" and grid[0][1]=="O" and grid[0][2]=="O":
             print("O wins")
             set (game_finsished = True)
-        elif grid[1]==["O", "O", "O"]:
+        elif grid[1][0] == "O" and grid[1][1]=="O" and grid[1][2]=="O":
             print("O wins")
             set (game_finsished = True)
-        elif grid[2]==["O", "O", "O"]:
+        elif grid[2][0] == "O" and grid[2][1]=="O" and grid[2][2]=="O":
             print("O wins")
             set (game_finsished = True)
         elif grid[0][0] == "X" and grid[1][0]=="X" and grid[2][0]=="X":
             print("X wins")
             set (game_finsished = True)
-        elif grid[0][1] == "X" and grid[1][2]=="X" and grid[2][3]=="X":
+        elif grid[0][1] == "X" and grid[1][2]=="X" and grid[2][2]=="X":
             print("X wins")
             set (game_finsished = True)
         elif grid[0][2] == "X" and grid[1][2]=="X" and grid[2][2]=="X":
@@ -35,7 +35,7 @@ def win(grid):
         elif grid[0][0] == "O" and grid[1][0]=="O" and grid[2][0]=="O":
             print("O wins")
             set (game_finsished = True)
-        elif grid[0][1] == "O" and grid[1][2]=="O" and grid[2][3]=="O":
+        elif grid[0][1] == "O" and grid[1][2]=="O" and grid[2][2]=="O":
             print("O wins")
             set (game_finsished = True)
         elif grid[0][2] == "O" and grid[1][2]=="O" and grid[2][2]=="O":
--- Work/final.py
+++ Work/final.py
@@ -50,55 +50,55 @@ print(taken)
 #Ryan R (The cool one) Win detector!
    
 def win(grid):
-        if list[0]==["X","X","X"]:
+        if  grid[0][0] == "X" and grid[0][1] =="X" and grid[0][2] =="X":
             print("X wins")
             set (game_finsished = True)
-        elif list[1]==["X", "X", "X"]:
+        elif grid[1][0] == "X" and grid[1][1] =="X" and grid[1][2] =="X":
             print("X wins")
             set (game_finsished = True)
-        elif list[2]==["X", "X", "X"]:
+        elif grid[2][0] == "X" and grid[2][1] =="X" and grid[2][2] =="X":
             print("X wins")
             set (game_finsished = True)
-        elif list[0]==["O", "O", "O"]:
+        elif grid[0][0] == "O" and grid[0][1] =="O" and grid[0][2] =="O":
             print("O wins")
             set (game_finsished = True)
-        elif list[1]==["O", "O", "O"]:
+        elif grid[1][0] == "O" and grid[1][1] =="O" and grid[1][2] =="O":
             print("O wins")
             set (game_finsished = True)
-        elif list[2]==["O", "O", "O"]:
+        elif grid[2][0] == "O" and grid[2][1] =="O" and grid[2][2] =="O":
             print("O wins")
             set (game_finsished = True)
-        elif list[0][0] == "X" and list[1][0]=="X" and list[2][0]=="X":
+        elif grid[0][0] == "X" and grid[1][0] =="X" and grid[2][0] =="X":
             print("X wins")
             set (game_finsished = True)
-        elif list[0][1] == "X" and list[1][2]=="X" and list[2][3]=="X":
+        elif grid[0][1] == "X" and grid[1][2] =="X" and grid[2][2] =="X":
             print("X wins")
             set (game_finsished = True)
-        elif list[0][2] == "X" and list[1][2]=="X" and list[2][2]=="X":
+        elif grid[0][2] == "X" and grid[1][2] =="X" and grid[2][2] =="X":
             print("X wins")
             set (game_finsished = True)
-        elif list[0][0] == "X" and list[1][1]=="X" and list[2][2]=="X":
+        elif grid[0][0] == "X" and grid[1][1] =="X" and grid[2][2] =="X":
             print("X wins")
             set (game_finsished = True)
-        elif list[0][2] == "X" and list[1][1]=="X" and list[2][0]=="X":
+        elif grid[0][2] == "X" and grid[1][1] =="X" and grid[2][0] =="X":
             print("X wins")
             set (game_finsished = True)
-        elif list[0][0] == "O" and list[1][0]=="O" and list[2][0]=="O":
+        elif grid[0][0] == "O" and grid[1][0]=="O" and grid[2][0]=="O":
             print("O wins")
             set (game_finsished = True)
-        elif list[0][1] == "O" and list[1][2]=="O" and list[2][3]=="O":
+        elif grid[0][1] == "O" and grid[1][2]=="O" and grid[2][2]=="O":
             print("O wins")
             set (game_finsished = True)
-        elif list[0][2] == "O" and list[1][2]=="O" and list[2][2]=="O":
+        elif grid[0][2] == "O" and grid[1][2]=="O" and grid[2][2]=="O":
             print("O wins")
             set (game_finsished = True)
-        elif list[0][0] == "O" and list[1][1]=="O" and list[2][2]=="O":
+        elif grid[0][0] == "O" and grid[1][1]=="O" and grid[2][2]=="O":
             print("O wins")
             set (game_finsished = True)
-        elif list[0][2] == "O" and list[1][1]=="O" and list[2][0]=="O":
+        elif grid[0][2] == "O" and grid[1][1]=="O" and grid[2][0]=="O":
             print("O wins")
             set (game_finsished = True)
-        elif list[0][0] == ("X" or "O") and list[0][1] == ("X" or "O") and list[0][2] == ("X" or "O") and list[1][0] == ("X" or "O") and list[1][1] == ("X" or "O") and list[1][2] == ("X" or "O") and list[2][0] == ("X" or "O") and list[2][1] == ("X" or "O") and list[2][2] == ("X" or "O"):
+        elif grid[0][0] == ("X" or "O") and grid[0][1] == ("X" or "O") and grid[0][2] == ("X" or "O") and grid[1][0] == ("X" or "O") and grid[1][1] == ("X" or "O") and grid[1][2] == ("X" or "O") and grid[2][0] == ("X" or "O") and grid[2][1] == ("X" or "O") and grid[2][2] == ("X" or "O"):
              print("Its a tie.")
              set (game_finsished = True)
         else:
