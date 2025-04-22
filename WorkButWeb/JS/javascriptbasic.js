730f06818d55210a45d9ff03eecfbce0c3d99190
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-04-22 09:51:00 -0600

    Worked on js

---
+++ WorkButWeb/JS/javascriptbasic.js
@@ -0,0 +1,13 @@
+function hello(){
+    document.getElementById("title").innerHTML = "Hello World!"
+}
+function change(){
+    document.getElementById("picture").innerHTML
+    height = "300px"
+}
+function highlight(){
+    document.getElementById("button").style.
+    backgroundColor = "orange"
+    document.getElementById("button").style.
+    color = "white"
+}
