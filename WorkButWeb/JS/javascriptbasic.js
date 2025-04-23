6caf6c83ffeeffeeabd536b54be4a5fd7b398547
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-04-23 09:23:24 -0600

    f

--- WorkButWeb/JS/javascriptbasic.js
+++ WorkButWeb/JS/javascriptbasic.js
@@ -1,5 +1,7 @@
 function hello(){
-    document.getElementById("title").innerHTML = "Hello World!"
+    let name = window.prompt("what is your name?")
+    document.getElementById("title").innerHTML = "Hello "
+    + name + "!"
 }
 function change(){
     document.getElementById("picture").innerHTML
@@ -7,7 +9,16 @@ function change(){
 }
 function highlight(){
     document.getElementById("button").style.
-    backgroundColor = "orange"
+    color = "orange"
+    document.getElementById("button").style.
+    color = "white"
+}
+function pop(){
+    window.alert("For real. Don't click the this!")
+}
+function push(){
+    document.getElementById("button").style.
+    color = "orange"
     document.getElementById("button").style.
     color = "white"
 }
