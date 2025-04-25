c45f50de169afa9f07095a7578e976ebc71011e8
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-04-25 09:52:27 -0600

    need to finish

---
+++ WorkButWeb/update4_fwp.js
@@ -0,0 +1,3 @@
+function display(){
+    if(document.getElementById("displayReasons").style.display != "felx"){document.getElementById("displayReasons").style.display = "block"}else{document.getElementById("dislist").innerHTML = "Show Less"}
+}
