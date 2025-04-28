97a38e09525b9d1c3877daa2a3bf8334a5d07d3c
RyanRowb <ryan.rowbury@ucas-edu.net>
2025-04-28 09:52:28 -0600

    Im tired grampa

--- WorkButWeb/update4_fwp.js
+++ WorkButWeb/update4_fwp.js
@@ -1,3 +1,12 @@
 function display(){
-    if(document.getElementById("displayReasons").style.display != "felx"){document.getElementById("displayReasons").style.display = "block"}else{document.getElementById("dislist").innerHTML = "Show Less"}
+    var extra = document.getElementById("extra");
+    var button = document.getElementById("dislist");
+    if(extra.style.display === "none"){extra.style.display = "block"; button.textContent = "Show Less"
+    }else{
+        extra.style.display = "none"
+        button.textContent= "Display the reasons you should go hunting?"
+    }
+}
+function change(){
+    document.getElementById("mamo").style.display = "none"; document.getElementById("mal").style.display = "block"
 }
